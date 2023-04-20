b7838ed3b658e354cf321311049dbc6410dd6a32
Joon kim <snsjoonkim@gmail.com>
2023-04-20 09:28:37 +0900

    Add ApiBean class

---
+++ src/main/java/io/utility/api/bean/ApiBean.java
@@ -0,0 +1,21 @@
+package io.utility.api.bean;
+
+public class ApiBean {
+
+	private String apiHttps;
+	private String authorization;
+
+	public String getApiHttps() {
+		return apiHttps;
+	}
+	public void setApiHttps(String apiHttps) {
+		this.apiHttps = apiHttps;
+	}
+	public String getAuthorization() {
+		return authorization;
+	}
+	public void setAuthorization(String authorization) {
+		this.authorization = authorization;
+	}
+
+}
